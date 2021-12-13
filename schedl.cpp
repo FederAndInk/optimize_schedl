535249ad6deba28e52418aa0db0c4d5ba1b5af71
Ludovic Jozeau <unifai@protonmail.com>
2021-12-13 20:45:02 +0100

    heuristics

--- schedl.cpp
+++ schedl.cpp
@@ -1,4 +1,8 @@
-#include <fmt/format.h>
+#include "utils.hpp"
+
+#include <fmt/core.h>
+
+#include <boost/range/adaptors.hpp>
 
 #include <algorithm>
 #include <cassert>
@@ -12,25 +16,37 @@
 
 namespace fai
 {
-using Index = int;
 using Sched_time = std::int64_t;
+using Cost = std::int64_t;
 } // namespace fai
 
 struct Task
 {
+  // number of the task
+  int no{};
   int exec_time{};
-  int weight{};
+  int weight{1};
   int expiry_time{};
 
   fai::Sched_time get_delay(fai::Sched_time start_time) const
   {
-    return std::max(fai::Sched_time{0},
-                    start_time + static_cast<fai::Sched_time>(exec_time - expiry_time));
+    return std::max(fai::Sched_time{0}, get_sdelay(start_time));
   }
 
-  fai::Sched_time get_cost(fai::Sched_time start_time) const
+  /**
+   * @brief Get the signed delay (no max with 0)
+   *
+   * @param start_time
+   * @return fai::Sched_time
+   */
+  fai::Sched_time get_sdelay(fai::Sched_time start_time) const
   {
-    return static_cast<fai::Sched_time>(weight) * get_delay(start_time);
+    return start_time + static_cast<fai::Sched_time>(exec_time - expiry_time);
+  }
+
+  fai::Cost get_cost(fai::Sched_time start_time) const
+  {
+    return static_cast<fai::Cost>(weight) * get_delay(start_time);
   }
 
   friend std::istream& operator>>(std::istream& in, Task& task)
@@ -50,19 +66,20 @@ std::vector<fai::Index> read_solution(std::istream& in, std::size_t nb_tasks)
 
 std::vector<Task> read_tasks(std::istream& in)
 {
-  std::size_t nb_task;
+  int nb_task;
   in >> nb_task;
   std::vector<Task> tasks;
   tasks.reserve(nb_task);
-  for (std::size_t i = 0; i < nb_task; ++i)
+  for (int i = 0; i < nb_task; ++i)
   {
     in >> tasks.emplace_back();
+    tasks.back().no = i;
   }
   return tasks;
 }
 
-fai::Sched_time evaluate(std::vector<Task> const&       tasks,
-                         std::vector<fai::Index> const& solution)
+fai::Cost evaluate(std::vector<Task> const&       tasks,
+                   std::vector<fai::Index> const& solution)
 {
   std::unordered_set<fai::Index> uniq_sol(std::begin(solution), std::end(solution));
   if (tasks.size() != uniq_sol.size())
@@ -93,17 +110,82 @@ std::vector<fai::Index> generate_random_solution(std::size_t nb_tasks)
   return sol;
 }
 
-template <typename T>
-std::ostream& operator<<(std::ostream& out, std::vector<T> const& v)
+template <typename Select_fn>
+std::vector<fai::Index> ct_heuristic(std::vector<Task> tasks, Select_fn&& f_select)
 {
-  std::cout << "{";
-  if (!v.empty())
+  fai::Sched_time         f = 0;
+  fai::Sched_time         curr_time = 0;
+  std::vector<fai::Index> sol;
+
+  while (!tasks.empty())
   {
-    std::copy(std::begin(v), std::end(v) - 1, std::ostream_iterator<T>(std::cout, ", "));
-    std::cout << v.back();
+    fai::Index task_idx = f_select(tasks, curr_time);
+    Task&      task = tasks[task_idx];
+    sol.push_back(task.no);
+    curr_time += task.exec_time;
+    if (task_idx != tasks.size() - 1)
+    {
+      std::swap(task, tasks.back());
+    }
+    tasks.pop_back();
   }
-  std::cout << "}";
-  return out;
+
+  return sol;
+}
+
+template <typename Eval_fn>
+auto select(Eval_fn&& eval_fn)
+{
+  return
+    [eval_fn = std::forward<Eval_fn>(eval_fn)](std::vector<Task> const& tasks,
+                                               fai::Sched_time curr_time) -> fai::Index
+  {
+    fai::Index i_min = 0;
+    auto       max_cost = eval_fn(tasks[i_min], curr_time);
+    for (auto&& [ind, task] : tasks | adp::indexed())
+    {
+      if (max_cost < eval_fn(task, curr_time))
+      {
+        i_min = ind;
+      }
+    }
+    return i_min;
+  };
+}
+
+long double eval_sdelay_divmul_weight(Task const& task, fai::Sched_time curr_time)
+{
+  if (task.get_sdelay(curr_time) < 0)
+  {
+    return task.get_sdelay(curr_time) / static_cast<long double>(task.weight);
+  }
+  else
+  {
+    return task.get_sdelay(curr_time) * task.weight;
+  }
+}
+
+long double eval_sdelay_div_weight(Task const& task, fai::Sched_time curr_time)
+{
+  return task.get_sdelay(curr_time) / static_cast<long double>(task.weight);
+}
+
+long double eval_static_sdelay_div_weight(Task const&                      task,
+                                          [[maybe_unused]] fai::Sched_time curr_time)
+{
+  return task.get_sdelay(0) / static_cast<long double>(task.weight);
+}
+
+long double eval_static_sdelay_mul_weight(Task const&                      task,
+                                          [[maybe_unused]] fai::Sched_time curr_time)
+{
+  return task.get_sdelay(0) * static_cast<long double>(task.weight);
+}
+
+long double eval_static_sdelay(Task const&                      task,
+                               [[maybe_unused]] fai::Sched_time curr_time)
+{
+  return task.get_sdelay(0);
 }
 
 int main(int argc, char** argv)
@@ -126,5 +208,34 @@ int main(int argc, char** argv)
     sol = generate_random_solution(tasks.size());
   }
   std::cout << "Scheduling: " << sol << "\n";
-  std::cout << "Total cost: " << evaluate(tasks, sol) << "\n";
+
+  try
+  {
+    std::locale::global(std::locale{"en_US.UTF-8"});
+  }
+  catch (std::runtime_error const& e)
+  {
+    std::cout << "Locale en_US.UTF-8 not on your system, falling back to C"
+              << "\n";
+    std::locale::global(std::locale{"C"});
+  }
+  fmt::print("Total cost: {:L}\n", evaluate(tasks, sol));
+
+  sol = ct_heuristic(tasks, select(eval_sdelay_divmul_weight));
+  fmt::print("Total cost eval_sdelay_divmul_weight heuristic: {:L}\n",
+             evaluate(tasks, sol));
+
+  sol = ct_heuristic(tasks, select(eval_sdelay_div_weight));
+  fmt::print("Total cost eval_sdelay_div_weight heuristic: {:L}\n", evaluate(tasks, sol));
+
+  sol = ct_heuristic(tasks, select(eval_static_sdelay_div_weight));
+  fmt::print("Total cost eval_static_sdelay_div_weight heuristic: {:L}\n",
+             evaluate(tasks, sol));
+
+  sol = ct_heuristic(tasks, select(eval_static_sdelay_mul_weight));
+  fmt::print("Total cost eval_static_sdelay_mul_weight heuristic: {:L}\n",
+             evaluate(tasks, sol));
+
+  sol = ct_heuristic(tasks, select(eval_static_sdelay));
+  fmt::print("Total cost eval_static_sdelay heuristic: {:L}\n", evaluate(tasks, sol));
 }
---
+++ utils.hpp
@@ -0,0 +1,55 @@
+#include <boost/range/adaptor/indexed.hpp>
+#include <boost/version.hpp>
+
+#include <iostream>
+#include <vector>
+
+#if BOOST_VERSION < 107400
+namespace std
+{
+
+#if defined(BOOST_CLANG)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmismatched-tags"
+#endif
+
+template <size_t N, class T, class Indexable>
+struct tuple_element<N, boost::range::index_value<T, Indexable>> :
+  boost::tuples::element<N, boost::range::index_value<T, Indexable>>
+{
+};
+
+template <class T, class Indexable>
+struct tuple_size<boost::range::index_value<T, Indexable>> :
+  std::integral_constant<std::size_t, 2>
+{
+};
+
+#if defined(BOOST_CLANG)
+#pragma clang diagnostic pop
+#endif
+
+} // namespace std
+#endif
+
+namespace fai
+{
+using Index = int;
+}
+
+namespace adp = boost::adaptors;
+
+using namespace std::literals;
+
+template <typename T>
+std::ostream& operator<<(std::ostream& out, std::vector<T> const& v)
+{
+  std::cout << "{";
+  if (!v.empty())
+  {
+    std::copy(std::begin(v), std::end(v) - 1, std::ostream_iterator<T>(std::cout, ", "));
+    std::cout << v.back();
+  }
+  std::cout << "}";
+  return out;
+}
